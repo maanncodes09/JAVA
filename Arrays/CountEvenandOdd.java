953e7365607efd8fbe0b13ba01204105c825d97d
Mansi Agarwal <mansiagarwal.1805@gmail.com>
2026-07-02 14:06:46 +0000

    Count Even and Odd

---
+++ Arrays/CountEvenandOdd.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class CountEvenandOdd {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int countOdd=0;
+        int countEven=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%2==0){
+                countEven++;
+            }
+            else{
+                countOdd++;
+            }
+        }
+        System.out.println(countEven+" "+countOdd);
+    }
+
+    
+}
