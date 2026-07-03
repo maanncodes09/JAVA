f4dd2ac1a9ccccf02b48be6b56f5e8238b6f7e2a
Mansi Agarwal <mansiagarwal.1805@gmail.com>
2026-07-03 02:10:32 +0000

    Linear Search

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.*;
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
+        }
+        boolean isFound=false;
+        int x=sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                isFound=true;
+                break;
+            }
+        }
+        System.out.println(isFound);
+        
+
+        
+    }
+    
+}
